38500310bbde158dad206c368e8b128341e10e5c
Carlos Fonseca <carlos_fonseca@tec.mx>
2020-03-06 19:20:55 -0600

    SpaceInvaders

---
+++ SpaceInvaders/Assets/Scripts/Enemigo.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private float limiteIzq, limiteDer, actual;
+    private int contador;
+    public int velocidad;
+    public GameObject balaE;
+    public int contadorD;
+    public int disparo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        actual = rb.position.x;
+        limiteIzq = actual - 1;
+        limiteDer = actual + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contador++;
+        contadorD++;
+        if (contadorD == disparo)
+        {
+            Instantiate(balaE, new Vector3(rb.position.x, rb.position.y - 1), new Quaternion(0, 0, 0, 0));
+            contadorD = 0;
+        }
+        if (actual < limiteDer && contador == velocidad)
+        {
+            actual++;
+            contador = 0;
+            rb.position = new Vector3(actual, rb.position.y);
+           
+
+        }
+        if (actual == limiteDer && contador == velocidad)
+        {
+            actual = actual - 2;
+            contador = 0;
+            rb.position = new Vector3(actual, rb.position.y);
+            //Instantiate(balaE, new Vector3(rb.position.x, rb.position.y - 1), new Quaternion(0, 0, 0, 0));
+        }
+
+
+    }
+}
---
+++ SpaceInvaders/Assets/Scripts/Navecita.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navecita : MonoBehaviour
+{
+    Rigidbody2D navecita;
+    public GameObject bala;
+    GameObject balaE;
+    float movimiento;
+    public float speed;
+    public int vida;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navecita = gameObject.GetComponent<Rigidbody2D>();
+        balaE = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimiento = Input.GetAxis("Horizontal") * speed;
+        navecita.MovePosition(new Vector2(navecita.position.x + movimiento, navecita.position.y));
+        if (Input.GetAxis("Jump") == 1.0&&balaE==null)
+        {
+           balaE= Instantiate(bala, new Vector3(navecita.position.x,navecita.position.y+1,bala.transform.position.z),new Quaternion(0,0,0,0));
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Choque contra " + collision.gameObject.tag);
+        if(collision.gameObject.tag == "balitaEnemiga")
+        {
+            //vida--;
+            //if (vida == 0)
+                Destroy(gameObject);
+        }
+    }
+
+}
